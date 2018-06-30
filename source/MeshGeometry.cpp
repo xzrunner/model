ba2830487bbb6be14b3d6dc5e675af8c1058f0be
zz <zhuguang@ejoy.com>
2018-06-30 20:39:29 +0800

    [FIXED] release render buffer

--- source/MeshGeometry.cpp
+++ source/MeshGeometry.cpp
@@ -8,7 +8,16 @@ namespace model
 
 MeshGeometry::~MeshGeometry()
 {
-	ur::Blackboard::Instance()->GetRenderContext().ReleaseVAO(vao, vbo, ebo);
+	auto& rc = ur::Blackboard::Instance()->GetRenderContext();
+	if (vao != 0) {
+		rc.ReleaseVAO(vao, vbo, ebo);
+	} else {
+		assert(vbo);
+		rc.ReleaseBuffer(ur::VERTEXBUFFER, vbo);
+		if (ebo != 0) {
+			rc.ReleaseBuffer(ur::INDEXBUFFER, ebo);
+		}
+	}
 }
 
 }
